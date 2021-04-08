6f9e9b4705c3a183395e59f866ea6bdbaa7792dd
Robert McCurdy <rwmccurdy@hotmail.com>
2021-04-07 23:26:27 -0700

    Implemented Slideshow on Home Page

--- reactapp/src/components/pages/web/Home.js
+++ reactapp/src/components/pages/web/Home.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 function Home() {
+
+  const slideImages = [
+    '../../img/green.jpg',
+    '../../img/gamesPreview.png',
+    '../../img/gamesPreview.png'
+  ];
+
   return (
     <div>
 
@@ -9,9 +18,29 @@ function Home() {
         <p className="welcome-text-description">Jump on the Bandwagon before it's too late!</p>
       </div>
 
-      <div className="container-info-grey">
+      {/* <div className="container-info-grey">
         <h2 className="container-info-text">Vote on games based on statistical analysis!</h2>
         <img className="container-info-image" src="../../../img/landingpage/games.png" alt="future games"/>
+      </div> */}
+
+      <div className="slideshow-container">
+        <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`, backgroundSize:'25vw', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+              <span className="slideshow-header-text">Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`, backgroundSize:'25vw', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+              <span className="slideshow-header-text">Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`, backgroundSize:'25vw', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+              <span className="slideshow-header-text">Slide 3</span>
+            </div>
+          </div>
+        </Slide>
       </div>
 
       {/* <div className="container-info-white">
